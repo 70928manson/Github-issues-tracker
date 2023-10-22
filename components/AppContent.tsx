414d48c5201b167176976572cc6369c300bf2815
Manson <manson972@yahoo.com>
2023-10-22 14:18:57 +0800

    use client at top component

--- components/AppContent.tsx
+++ components/AppContent.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import { useSelector } from 'react-redux';
--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { format } from 'date-fns';
 import { motion } from 'framer-motion';
 import toast from 'react-hot-toast';
