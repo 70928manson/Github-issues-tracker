b4f2afd9c7f6151f246a0d6e26e836397872a6de
Manson <manson972@yahoo.com>
2023-11-09 20:09:51 +0800

    update login button

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { ReactNode } from 'react';
 
 import "../styles/components/button.css";
@@ -7,18 +9,16 @@ import { BsGithub } from 'react-icons/bs';
 import { signIn, useSession } from 'next-auth/react';
 
 interface ILoginButtonProps {
-    // type?: string;
-    // variant: string;
     children: ReactNode;
-    //onClick?: () => void;
 }
 
 const LoginButton: React.FC<ILoginButtonProps> = ({ children }) => {
     return (
         <button
             className="login-button login-button--social-login login-button--github mx-auto cursor-pointer"
-            onClick={() => {
-                signIn("github")
+            onClick={(e) => {
+                e.preventDefault();  //!!!!!!!!!!!!!!!!超重要
+                signIn("github");
             }}
         >
             {/* <i className="icon fa fa-github"></i> */}
