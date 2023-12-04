5e81f4bcbc08a2f9782cfdc1793e14b1258dd88e
Manson <manson972@yahoo.com>
2023-12-04 20:05:56 +0800

    update navbar component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import { Providers } from './providers/Providers'
+import Navbar from '@/components/Navbar';
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className}`}>
         <Providers>
+          <Navbar />
           {children}
         </Providers>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import AppContent from '@/components/AppContent'
 import AppHeader from '@/components/AppHeader'
-import Navbar from '@/components/Navbar';
 import Image from 'next/image'
 import { Toaster } from 'react-hot-toast';
 
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
       <main className="w-[90%] max-w-[1200px] mx-auto">
         <h1 className="inline-block w-full text-4xl font-bold uppercase text-center mx-auto mt-8 mb-6 text-black md:text-5xl">
           Github issue tracker
