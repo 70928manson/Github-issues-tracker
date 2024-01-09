e2a61c2165f937dbba76f00b24bcfe16dc2d12a5
70928manson <manson972@yahoo.com>
2024-01-09 15:08:52 +0800

    feat: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import { Providers } from './providers/Providers'
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <>
+            <div className="text-center p-12">
+                © 2024 Copyright: 
+                <a className="font-bold no-underline pl-2" href='https://github.com/70928manson' target="_blank">
+                    70928manson
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Footer
